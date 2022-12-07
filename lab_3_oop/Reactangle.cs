01d8a73e4cfc79987c1e83518d6e756182d85584
NikitaLipovatyi21 <120053458+NikitaLipovatyi21@users.noreply.github.com>
2022-12-07 21:42:17 +0100

    Add files via upload

---
+++ lab_3_oop/Reactangle.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3_1_oop
+{
+    public class Rectangle
+    {
+        public double side1;
+        public double side2;
+        public double area;
+        public double perimeter;
+        public string choice1;
+        public string choice2;
+        public string rechangesides;
+
+        double Area
+        {
+            get { return this.area; }
+        }
+
+        double Perimeter
+        {
+            get { return this.perimeter; }
+        } 
+        public Rectangle (double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+       public double AreaCalculator()
+        {
+            return this.area =  side1 * side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return this.perimeter =  (side1 * 2) + (side2 * 2);
+        }
+
+        public void ChoiceOne()
+        {
+            switch (this.choice1)
+            {
+                case "+":
+                    PerimeterCalculator();
+                    PrintPerimeter();
+                    break;
+                case "-":
+                    Console.WriteLine("Шкода!! А ми так для вас старались(");
+                    break;
+                default:
+                    Console.WriteLine("Ви не ввели + або -, тому зроблю вигляд, що я цього не помiтив)");
+                    break;
+            }
+        }
+
+        public void ChoiceTwo()
+        {
+            switch (this.choice2)
+            {
+                case "+":
+                    AreaCalculator();
+                    PrintArea();
+                    break;
+                case "-":
+                    Console.WriteLine("Шкода!! А ми так для вас старались(");
+                    break;
+                default:
+                    Console.WriteLine("Ви не ввели + або -, тому зроблю вигляд, що я цього не помiтив)");
+                    break;
+            }
+        }
+        public void RechangeSides()
+        {
+            switch (this.rechangesides)
+            {
+                case "+":
+                    break;
+                case "-":
+                    PerimeterCalculator();
+                    PrintPerimeter();
+                    AreaCalculator();
+                    PrintArea();
+                    break;
+                default:
+                    Console.WriteLine("Ви не ввели + або -, тому комп'ютер це побачив як + ))))");
+                    break;
+            }
+        }
+
+        public void DoubleCheckOne()
+        {
+            do
+            {
+               
+            }
+            while (!double.TryParse(Console.ReadLine(), out this.side1) || this.side1<0);
+        }
+        public void DoubleCheckTwo()
+        {
+            do
+            {
+               
+            }
+            while (!double.TryParse(Console.ReadLine(), out this.side2) || this.side2<0);
+        }
+
+               public void PrintArea()
+        {
+            Console.WriteLine($"Площа дорiвнює {Area} см");
+        }
+        public void PrintPerimeter()
+        {
+            Console.WriteLine($"Периметр дорiвнює {Perimeter} см");
+        }
+    }
+}
---
+++ lab_3_oop/task_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_3_1_oop
+{
+    class task_1
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10,5);
+            Console.WriteLine("Вас вiтає калькулятор площi та периметра прямокутника!");
+            Console.WriteLine("На даний момент сторони запрограмованi як 10 см та 5 см, бажаєте змiнити?");
+            Console.WriteLine("Напишiть +, якщо так, та -, якщо нi");
+            rectangle.rechangesides = Console.ReadLine();
+            rectangle.RechangeSides();
+            Console.WriteLine("Введiть першу сторонy прямокутника:");
+            rectangle.DoubleCheckOne();
+            Console.WriteLine("Супер! Тепер Введiть другу сторону:");
+            rectangle.DoubleCheckTwo();
+            Console.WriteLine("Бажаєте дiзнатись периметр цього прямокутника?");
+            Console.WriteLine("Напишiть +, якщо так, та -, якщо нi");
+            rectangle.choice1 = Console.ReadLine();
+            rectangle.ChoiceOne();
+            Console.WriteLine("Бажаєте дiзнатись площу цього прямокутника?");
+            Console.WriteLine("Напишiть +, якщо так, та -, якщо нi");
+            rectangle.choice2 = Console.ReadLine();
+            rectangle.ChoiceTwo();
+            Console.WriteLine("Дякую, що завiтали!!!");
+            Console.ReadKey();
+        }
+    }
+}
